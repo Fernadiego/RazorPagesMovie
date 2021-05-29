afc58b0365070d9d4e44dbcf9f7f77f03b70d3c8
Fernando Peloso <fernadiego@hotmail.com>
2021-05-29 14:47:20 +0200

    Add project files.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        /// <summary>
+        /// La base de datos requiere el campo para la clave principal.
+        /// </summary>
+        public int ID { get; set; }
+        public string Title { get; set; }
+        /// <summary>
+        /// El atributo [DataType] especifica el tipo de datos (Date). Con este atributo:
+        /// No se requiere que el usuario ingrese información de hora en el campo de fecha.
+        /// Solo se muestra la fecha, no la información de la hora.
+        /// </summary>
+        
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")] 
+        public decimal Price { get; set; }
+    }
+}
---
+++ RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public CreateModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// El OnGetmétodo inicializa cualquier estado necesario para la página.
+        /// </summary>
+        /// <returns>La página Crear no tiene ningún estado para inicializar, por lo que Page se devuelve.</returns>
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        /// <summary>
+        /// La Movie propiedad utiliza el atributo [BindProperty] para optar por la vinculación del modelo.
+        /// </summary>
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        /// <summary>
+        /// El OnPostAsyn cmétodo se ejecuta cuando la página publica datos del formulario.
+        /// </summary>
+        /// <returns></returns>
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
